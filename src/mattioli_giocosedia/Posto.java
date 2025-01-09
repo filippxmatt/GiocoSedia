fb2bc82a4e637407a6c346e7c17b569bd8114ce1
Filippo Mattioli <filippomattioli06@gmail.com>
2025-01-09 11:15:07 +0100

    primo upload

---
+++ src/mattioli_giocosedia/Posto.java
@@ -0,0 +1,24 @@
+package mattioli_giocosedia;
+
+class Posto
+
+{
+	private boolean occupato;
+
+	public Posto()
+
+	{
+		occupato = false;
+	}
+
+	public synchronized boolean libero() {
+		return (!occupato);
+	}
+
+	public synchronized boolean occupa() {
+		if (occupato)
+			return false;
+		else
+			return occupato = true;
+	}
+}
